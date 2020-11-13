85910fbd9a8106b756ccedf7650a508b4a5125ea
Jeff Reiner <gone@hey.com>
2020-11-13 21:32:52 +0100

    Custom login-pin page

---
+++ pages/login-pin.js
@@ -0,0 +1,12 @@
+export default function LoginPin() {
+  return (
+    <main className="main">
+      <h1>
+        Logging you in...{" "}
+        <span role="img" aria-label="Soap">
+          🧼
+        </span>
+      </h1>
+    </main>
+  );
+}
