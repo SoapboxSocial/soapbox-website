b215c22844495ab097b37a94664a5f8610ed7a95
Jeff Reiner <theundeadghost@gmail.com>
2021-01-19 14:34:08 +0100

    Remove nav variant

--- components/header.js
+++ components/header.js
@@ -1,27 +1,6 @@
 import Link from "next/link";
 
-export default function Header({ isNavigable = true, isWide = true }) {
-  if (isWide) {
-    return (
-      <header className="mt-8 max-w-2xl mx-auto w-full">
-        <nav className="flex items-center justify-between">
-          <Link href="/">
-            <a>
-              <img className="brand-mark" src="/brand.png" alt="Soapbox" />
-            </a>
-          </Link>
-
-          <a
-            href="https://apps.apple.com/us/app/soapbox-talk-with-anyone/id1529283270"
-            className="room-button"
-          >
-            App Store
-          </a>
-        </nav>
-      </header>
-    );
-  }
-
+export default function Header({ isNavigable = true }) {
   return (
     <header className="mt-8">
       <nav className="flex justify-center">
