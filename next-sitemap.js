820bc65c4dfc1fa048f8e55a0240ddcf33f5f238
Jeff Reiner <theundeadghost@gmail.com>
2021-01-20 13:21:21 +0100

    Sitemap ignoring rooms

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,17 @@
 module.exports = {
   siteUrl: "https://soapbox.social",
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        disallow: ["/room"],
+      },
+    ],
+  },
   exclude: ["/login-pin", "/report/incident", "/report/received", "/room/*"],
 };
