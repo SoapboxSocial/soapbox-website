3953f3dcbfcf971e7ad7edda76d639e72d5dce76
Jeff Reiner <theundeadghost@gmail.com>
2021-01-25 16:44:32 +0100

    Adding redirect

--- next.config.js
+++ next.config.js
@@ -4,4 +4,14 @@ module.exports = withMDX({
   images: {
     domains: ["cdn.soapbox.social"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/deck",
+        destination:
+          "https://app.pitch.com/app/presentation/18786779-94a0-468a-b077-46bf99e9cc7b/32695aa9-d706-4579-a514-702222601d84",
+        permanent: false,
+      },
+    ];
+  },
 });
