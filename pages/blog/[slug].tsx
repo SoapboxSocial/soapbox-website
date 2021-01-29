4d610c19ef3cabc304e1b8dea72fca3cf4c8580d
Jeff Reiner <theundeadghost@gmail.com>
2021-01-29 14:07:59 +0100

    Working on layout

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,5 +1,6 @@
 import type { GetStaticPaths, InferGetStaticPropsType } from "next";
 import hydrate from "next-mdx-remote/hydrate";
+import RoomPreview from "../../components/room-preview";
 import { getAllPosts, getPostBySlug } from "../../lib";
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
@@ -9,10 +10,26 @@ export default function Post({ slug, meta, source }: Props) {
 
   return (
     <main className="flex-1 p-5">
-      <div className="prose mx-auto">
-        <h1>{meta?.title ?? "No Title"}</h1>
+      <div className="max-w-lg mx-auto">
+        <RoomPreview
+          roomName={meta.title}
+          roomMembers={[
+            {
+              displayName: "Dean",
+              image: "/dean.png",
+            },
+            {
+              displayName: "Jeff",
+              image: "/jeff.png",
+            },
+          ]}
+          buttonText="Feb 3, 2020"
+          buttonLink=""
+        />
 
-        {content}
+        <div className="h-5" />
+
+        <div className="prose sm:px-5 crop">{content}</div>
       </div>
     </main>
   );
