64732e7fb30945cbdb9354193e452b22ce3ed85b
Jeff Reiner <theundeadghost@gmail.com>
2021-02-01 14:14:13 +0100

    Adding some Footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,6 @@
-import Meta from "../components/meta";
-import { APP_STORE_URL } from "../constants";
-import Apple from "../icons/apple";
 import Image from "next/image";
-
-function AppStoreButton() {
-  return (
-    <a
-      className="py-4 px-6 bg-brand text-white rounded-full inline-flex justify-center items-center space-x-3 focus:outline-none focus:ring-4"
-      href={APP_STORE_URL}
-      aria-label="Download on the App Store"
-    >
-      <Apple size={32} className="-mt-0.5" />
-      <span className="text-2xl font-medium">App Store</span>
-    </a>
-  );
-}
+import AppStoreButton from "../components/app-store-button";
+import Meta from "../components/meta";
 
 export default function HomePage() {
   return (
