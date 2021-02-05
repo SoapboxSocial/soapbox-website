08dc54cbed77442fcb690fb2ac2da59f72588dfa
Jeff Reiner <theundeadghost@gmail.com>
2021-02-05 16:22:29 +0100

    New Website Homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,81 +4,137 @@ import Meta from "../components/meta";
 
 export default function HomePage() {
   return (
-    <main className="p-5">
+    <main className="max-w-6xl mx-auto p-2.5 sm:p-5 grid lg:grid-cols-2 gap-2.5 sm:gap-5">
       <Meta />
 
-      <div className="max-w-5xl w-full mx-auto pt-5 md:pt-10 px-5 md:px-10 bg-white overflow-hidden text-center relative rounded-room">
-        <div className="grid lg:grid-cols-2 gap-16">
-          <div className="text-center lg:text-left">
-            <img
-              draggable={false}
-              className="soapbox-mark mx-auto lg:mx-0"
-              src="/brand.png"
-              alt="Soapbox"
-              loading="eager"
-            />
+      <div className="bg-white rounded-room relative overflow-hidden lg:col-span-2">
+        <div className="pt-10 px-10">
+          {" "}
+          <div className="grid lg:grid-cols-2 gap-8">
+            <div className="text-center lg:text-left">
+              <img
+                alt="Soapbox"
+                className="soapbox-mark mx-auto lg:mx-0"
+                draggable={false}
+                loading="eager"
+                src="/brand.png"
+              />
 
-            {/* Spacer */}
-            <div className="h-8" />
+              {/* Spacer */}
+              <div className="h-8" />
 
-            <h1 className="text-4xl font-bold">Soapbox</h1>
+              <h1 className="text-4xl font-bold">Soapbox</h1>
 
-            {/* Spacer */}
-            <div className="h-4" />
+              {/* Spacer */}
+              <div className="h-4" />
 
-            <p className="text-xl">
-              A drop-in audio chat app to have casual conversations with
-              strangers and friends.
-            </p>
+              <p className="max-w-xs mx-auto lg:mx-0 text-xl text-gray-700">
+                A drop-in audio chat app to have casual conversations with
+                strangers and friends.
+              </p>
 
-            {/* Spacer */}
-            <div className="h-16" />
+              {/* Spacer */}
+              <div className="h-16" />
 
-            <div className="flex justify-center">
-              <div className="relative inline-block mx-auto">
-                <AppStoreButton />
+              <div className="flex justify-center">
+                <div className="relative inline-block mx-auto">
+                  <AppStoreButton />
 
-                <img
+                  <img
+                    draggable={false}
+                    aria-hidden
+                    className="absolute w-40 -right-44 top-4 transform-gpu -rotate-12"
+                    src="/cyan-dude.png"
+                    alt=""
+                  />
+
+                  <img
+                    draggable={false}
+                    aria-hidden
+                    className="absolute -left-36 -top-10 transform-gpu -rotate-12 w-32"
+                    src="/green-dude.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="flex justify-center lg:pt-12">
+              <div className="w-8/12 sm:w-auto flex">
+                <Image
+                  alt="Soapbox App"
                   draggable={false}
-                  aria-hidden
-                  className="absolute w-40 -right-44 top-4 transform-gpu -rotate-12"
-                  src="/cyan-dude.png"
-                  alt=""
-                />
-
-                <img
-                  draggable={false}
-                  aria-hidden
-                  className="absolute -left-36 -top-10 transform-gpu -rotate-12 w-32"
-                  src="/green-dude.png"
-                  alt=""
+                  height={534}
+                  objectFit="contain"
+                  priority
+                  src="/screenshot.png"
+                  width={360}
                 />
               </div>
             </div>
           </div>
-
-          <div className="flex justify-center lg:pt-12">
-            <Image
-              src="/screenshot.png"
-              width={390}
-              height={620}
-              draggable={false}
-              priority
-              objectFit="cover"
-              objectPosition="top"
-              className="align-middle"
-              alt=""
-            />
-          </div>
         </div>
 
         <img
-          draggable={false}
+          alt=""
           aria-hidden
           className="absolute -right-4 md:right-8 -bottom-4 md:bottom-8 transform-gpu -rotate-12 w-32"
+          draggable={false}
           src="/pink-dude.png"
-          alt=""
         />
+      </div>
+
+      <div className="bg-white rounded-room">
+        <div className="p-10">
+          <div className="max-w-xs mx-auto text-center">
+            <h2 className="text-3xl font-bold">Share Something Cool</h2>
+
+            {/* Spacer */}
+            <div className="h-2" />
+
+            <p className="text-lg text-gray-700">
+              Share links, videos, and more in rooms to keep the conversation
+              going.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex justify-center">
+          <Image
+            alt="Link Sharing"
+            draggable={false}
+            height={400}
+            objectFit="contain"
+            src="/link-share.png"
+            width={375}
+          />
+        </div>
+      </div>
+
+      <div className="bg-white rounded-room relative overflow-hidden">
+        <div className="-mt-4 h-72 relative">
+          <Image
+            alt="Stories"
+            draggable={false}
+            layout="fill"
+            objectFit="cover"
+            src="/story-heads.png"
+          />
+        </div>
+
+        <div className="p-10">
+          <div className="max-w-xs mx-auto text-center">
+            <h2 className="text-3xl font-bold">Connect Any Time</h2>
+
+            {/* Spacer */}
+            <div className="h-2" />
+
+            <p className="text-lg text-gray-700">
+              Want to let your friends know how you're doing? Post a voice
+              story.
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
