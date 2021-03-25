aea121745c38f35b7e2ad097ca94ae28dc583f2a
Jeff Reiner <theundeadghost@gmail.com>
2021-03-25 11:06:18 +0100

    Track AppStore click events on homepage

--- components/app-store-button.tsx
+++ components/app-store-button.tsx
@@ -1,12 +1,23 @@
+import { trackGoal } from "fathom-client";
+import type { MouseEvent } from "react";
 import { APP_STORE_URL } from "../constants";
 import Apple from "../icons/apple";
 
 export default function AppStoreButton() {
+  const handleOnClick = (e: MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+
+    trackGoal("NUSYSEKO", 0);
+
+    window.location.href = e.currentTarget.href;
+  };
+
   return (
     <a
       className="py-4 px-6 bg-soapbox text-white rounded-full inline-flex justify-center items-center space-x-3 focus:outline-none focus:ring-4"
       href={APP_STORE_URL}
       aria-label="Download on the App Store"
+      onClick={handleOnClick}
     >
       <Apple size={32} className="-mt-0.5" />
       <span className="text-2xl font-medium">App Store</span>
