333249c1596bce73c6eaf5ea9311d716e7e2dc1e
Jeff Reiner <theundeadghost@gmail.com>
2021-04-01 17:38:24 +0200

    Update blog design

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,8 +1,8 @@
 import dayjs from "dayjs";
 import type { InferGetStaticPropsType } from "next";
+import Image from "next/image";
 import Link from "next/link";
 import Meta from "../../components/meta";
-import PostPreview from "../../components/post-preview";
 import { getAllPostsWithData } from "../../lib";
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
@@ -12,33 +12,65 @@ export default function Blog({ posts }: Props) {
     <main className="p-5">
       <Meta title="Our Soapbox" url="https://soapbox.social/blog" />
 
-      <div className="max-w-sm mx-auto">
+      <div className="max-w-lg mx-auto">
         <div className="text-center">
           <h1 className="text-4xl font-extrabold leading-none text-prose-primary">
             Our Soapbox
           </h1>
 
-          <div className="h-4" />
+          <div className="h-2" />
 
           <p className="text-prose-secondary">A blog by the Soapbox team.</p>
         </div>
 
         <div className="h-8" />
 
-        <ul className="space-y-5">
+        <ul className="space-y-12">
           {posts
             .sort((a, b) => (dayjs(a.meta.date).isAfter(b.meta.date) ? -1 : 1))
             .map(({ slug, meta }, i) => (
               <li key={i}>
-                <Link href={`/blog/${slug}`}>
-                  <a>
-                    <PostPreview date={meta.date} title={meta.title} />
-                  </a>
-                </Link>
+                <article className="space-y-2">
+                  <p className="text-sm leading-none text-gray-400">
+                    {dayjs(meta.date).format("MMMM D, YYYY")}
+                  </p>
+
+                  <Link href={`/blog/${slug}`}>
+                    <a className="block ">
+                      <h2 className="text-2xl font-bold">{meta.title}</h2>
+                    </a>
+                  </Link>
+
+                  {typeof meta.thumb === "string" && (
+                    <Link href={`/blog/${slug}`}>
+                      <a className="flex">
+                        <Image
+                          className="bg-soapbox"
+                          src={meta.thumb}
+                          width={640}
+                          height={320}
+                          objectFit="cover"
+                          objectPosition="top"
+                          alt={meta.title}
+                        />
+                      </a>
+                    </Link>
+                  )}
+
+                  {typeof meta.summary === "string" && (
+                    <p className="text-base text-gray-500">{meta.summary}</p>
+                  )}
+                </article>
               </li>
             ))}
         </ul>
       </div>
+
+      <style jsx global>{`
+        body {
+          background-color: #fff;
+        }
+      `}</style>
     </main>
   );
 }
