a7ce4b3063f337dac29a2c9dacda60c644abf0ef
Jeff Reiner <theundeadghost@gmail.com>
2021-04-22 20:28:06 +0200

    Blog

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,18 +1,48 @@
 import type { MdxRemote } from "next-mdx-remote/types";
 import Image from "next/image";
+import Link from "next/link";
 import { TwitterTweetEmbed } from "react-twitter-embed";
 
 export const mdxComponents: MdxRemote.Components = {
-  TwitterTweetEmbed,
+  TwitterTweetEmbed: (props) => (
+    <div className="tweet">
+      <TwitterTweetEmbed {...props} />
+
+      <style jsx global>{`
+        .tweet > div {
+          display: flex;
+          justify-content: center;
+        }
+      `}</style>
+    </div>
+  ),
   img: ({ src, alt }) => (
     <Image
       width={640}
       height={498}
       src={src}
       alt={alt ?? ""}
+      className="bg-soapbox"
       key={src}
       objectFit="cover"
       objectPosition="center"
     />
   ),
+  a: ({ href, children }) => {
+    const isExternalLink = href.startsWith("http");
+
+    if (isExternalLink) {
+      return (
+        <a href={href} target="_blank" rel="noopener noreferrer">
+          {children}
+        </a>
+      );
+    }
+
+    return (
+      <Link href={href}>
+        <a>{children}</a>
+      </Link>
+    );
+  },
 };
---
+++ posts/sharing-is-caring.md
@@ -0,0 +1,18 @@
+---
+title: Sharing is Caring
+date: "2021-04-22"
+summary: Lorem ipsum dolor sit amet
+thumb: /blog/new-share-sheet.png
+---
+
+After we released [Draw with Friends](/blog/draw-with-friends), we needed to make it easier to invite them!
+
+We’ve shortened the links to profiles and rooms with soap.link/_@your-username_ e.g [soap.link/@mikec](https://soap.link/@mikec) and soap.link/_room-unique-id_, and made it even easier to share the room you’re in to other apps.
+
+![New Share Sheet](/blog/new-share-sheet.png)
+
+Now when you share a link to the room you’re in you’ll see a new and improved preview.
+
+<TwitterTweetEmbed tweetId="1384219088432496644" />
+
+We hope these new improvements expand the reach of rooms and make it easier to compete with friends outside of Soapbox.
